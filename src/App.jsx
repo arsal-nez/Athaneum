2a3ad7f3a2e1ab7a3000ad163f4e359c021bf451
MD ARSALAN <mdarsalanbgp786@gmail.com>
2024-09-01 01:00:09 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import './App.css';
 import Letter from './components/Letter';
 import Home from './components/Home';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/letter" element={<Letter />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
